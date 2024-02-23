194a8b140208f7ce0c75159d8ca362ea10725b34
Aleksandr Selivanov <151870448+smalltown-boy@users.noreply.github.com>
2024-02-23 16:12:25 +0300

    Протестирован механизм создания базы данных users (для пользователей).

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from PyQt6 import QtWidgets
 import gui.login as login
 import gui.register as register
+from source.database import Database
 
 class WindowLogin(QtWidgets.QMainWindow, login.Ui_login_form): # Класс, вызывающий окно регистрации и входа
     def __init__(self,parent=None): # Функция инициализации
@@ -22,9 +23,13 @@ class WindowRegister(QtWidgets.QMainWindow, register.Ui_register_form):
         QtWidgets.QWidget.__init__(self,parent)
         self.setupUi(self)
         self.abort_button.clicked.connect(self.exit_register_window) # Задаём событие для кнопки отмены
+        self.register_button.clicked.connect(self.user_register) # Задаём событие для кнопки регистрации
     def exit_register_window(self): # Функция выхода из окна регистрации
         self.close()                # Закрываем окно регистрации
         window.show()               # Открываем окно входа
+    def user_register(self): # Функция регистрации нового пользователя
+        self.db = Database() # Создаём объект базы данных
+        self.db.database_open()       # Открываем баз данных
 
 
 if __name__ == "__main__":
