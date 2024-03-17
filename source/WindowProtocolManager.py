59b0498168180ce8994b64e134670803ed6cc9c0
Aleksandr Selivanov <151870448+smalltown-boy@users.noreply.github.com>
2024-03-17 12:58:16 +0300

    Начата реализация сохранения введённых пользователем коман.
    Пока только заготовка.

--- source/WindowProtocolManager.py
+++ source/WindowProtocolManager.py
@@ -19,9 +19,9 @@ class WindowProtocolManager(QtWidgets.QDialog,
         self.btn_save_protocol.clicked.connect(self.save_protocol)  # Кнопка сохранения протокола
         self.checkBox_no_answer.stateChanged.connect(self.check_box)  # Функция для вызова чек бокса
         # Переменная для хранения протокола
-        command_flow = {}
+        self.command_flow = {}
         # Переменная для чек бокса
-        check_box = False
+        self.check_box = False
 
     def register_user_data(self, user_data):  # Сохраняем данные о пользователе
         self.user_data = user_data  # Получаем данные пользователя
@@ -31,7 +31,20 @@ class WindowProtocolManager(QtWidgets.QDialog,
         command_request = self.line_request.text()
         command_answer = self.line_answer.text()
 
-        
+        if not self.check_box:  # Если чек бокс не поставлен
+            if all([command_name, command_request, command_answer]):  # Если все поля заполнены
+                # Сохраняем данные
+                pass
+            else:
+                # Выводим сообщение об ошибке
+                print("Заглушка для ошибки, если не заполнены все поля.")
+        else:  # Если чек бокс поставлен
+            if all([command_name, command_request]):  # Если два первых поля заполнены
+                # Сохраняем данные
+                pass
+            else:
+                # Выводим сообщение об ошибке
+                print("Заглушка для ошибки, если не заполнены первые два поля.")
 
     def delete_command(self):
         pass
@@ -47,8 +60,8 @@ class WindowProtocolManager(QtWidgets.QDialog,
 
     def check_box(self):
         if self.checkBox_no_answer.isChecked():
-            check_box = True
+            self.check_box = True
             self.line_answer.setReadOnly(True)  # Делаем окно ввода ответа недоступным
         else:
-            check_box = False
+            self.check_box = False
             self.line_answer.setReadOnly(False)  # Делаем окно ввода ответа доступным
